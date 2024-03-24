b743942b0c813c0bd9d91f5e0e17560ec45e328f
Youjin Hwang <rgh747052@gmail.com>
2024-03-25 01:20:45 +0900

    update

--- src/component/WeatherButton.js
+++ src/component/WeatherButton.js
@@ -1,17 +1,16 @@
 import React from 'react'
 import { Button } from 'react-bootstrap';
 
-const WeatherButton=({cities, setCity})=> {
+const WeatherButton=({cities,selectedCity, setCity})=> {
     console.log("cities",cities);
-    
 
-    
     return (
-        <div>
-            <Button variant="warning" onClick={()=>setCity("")}>Current Location</Button>
+        <div class="menu-container">
+            <Button variant={`${selectedCity ==="" ?"outline-warning":"warning"}`} 
+            onClick={()=>setCity("")}>Current Location</Button>
             
             {cities.map((item,index)=>(
-                <Button variant="warning" 
+                <Button variant={`${selectedCity == item ? "outline-warning" : "warning"}`} 
                 key={index}
                 onClick={()=>setCity(item)}
                 >{item}</Button>
